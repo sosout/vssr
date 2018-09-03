0ac94ae49f5f461af83bad2c55923f8a5685eecf
sosout <sosout@139.com>
2018-09-03 17:27:27 +0800

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+/*!
+ * Vssr.js
+ * (c) 2018 Weich
+ * Released under the MIT License.
+ */
+
+const fs = require('fs')
+const path = require('path')
+
+if (fs.existsSync(path.resolve(__dirname, '.babelrc'))) {
+  // TODO: Use esm version when using linked repository to prevent builds
+  const requireModule = require('esm')(module, {})
+  module.exports = requireModule('./lib/index.js').default // TODO: default?
+} else {
+  // 默认情况下使用生产包
+  module.exports = require('./dist/vssr.js')
+}
